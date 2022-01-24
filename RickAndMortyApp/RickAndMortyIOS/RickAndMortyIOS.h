61a4b86f697eb60a432e2095b43d0386a936e6c3
FlorianTo <florian.tousch@hotmail.fr>
2022-01-24 12:02:02 +0100

    Add `RickAndMortyIOS` framework target for iOS platform specific component

---
+++ RickAndMortyApp/RickAndMortyIOS/RickAndMortyIOS.h
@@ -0,0 +1,18 @@
+//
+//  RickAndMortyIOS.h
+//  RickAndMortyIOS
+//
+//  Created by Florian on 24/01/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RickAndMortyIOS.
+FOUNDATION_EXPORT double RickAndMortyIOSVersionNumber;
+
+//! Project version string for RickAndMortyIOS.
+FOUNDATION_EXPORT const unsigned char RickAndMortyIOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RickAndMortyIOS/PublicHeader.h>
+
+
